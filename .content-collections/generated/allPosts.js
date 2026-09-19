0a06649b2e04239236c30007d460ea5f50203d12
Abhijit47 <akarmakar846@gmail.com>
2026-09-19 20:58:08 +0530

    chore(config): track .content-collections directory in git
    Remove .content-collections from .gitignore to ensure the generated
    collection metadata is tracked within the repository.

---
+++ .content-collections/generated/allPosts.js
@@ -0,0 +1,20 @@
+
+export default [
+  {
+    "title": "Hello world",
+    "summary": "This is my first post!",
+    "content": "## Hello world\r\n\r\nThis is my first post!\r\n... rest of the content",
+    "draft": false,
+    "_meta": {
+      "filePath": "hello-world.md",
+      "fileName": "hello-world.md",
+      "directory": ".",
+      "extension": "md",
+      "path": "hello-world"
+    },
+    "mdx": "var Component=(()=>{var m=Object.create;var s=Object.defineProperty;var x=Object.getOwnPropertyDescriptor;var u=Object.getOwnPropertyNames;var _=Object.getPrototypeOf,f=Object.prototype.hasOwnProperty;var p=(n,t)=>()=>(t||n((t={exports:{}}).exports,t),t.exports),j=(n,t)=>{for(var o in t)s(n,o,{get:t[o],enumerable:!0})},i=(n,t,o,c)=>{if(t&&typeof t==\"object\"||typeof t==\"function\")for(let r of u(t))!f.call(n,r)&&r!==o&&s(n,r,{get:()=>t[r],enumerable:!(c=x(t,r))||c.enumerable});return n};var w=(n,t,o)=>(o=n!=null?m(_(n)):{},i(t||!n||!n.__esModule?s(o,\"default\",{value:n,enumerable:!0}):o,n)),M=n=>i(s({},\"__esModule\",{value:!0}),n);var h=p((C,l)=>{l.exports=_jsx_runtime});var g={};j(g,{default:()=>d});var e=w(h());function a(n){let t={h2:\"h2\",p:\"p\",...n.components};return(0,e.jsxs)(e.Fragment,{children:[(0,e.jsx)(t.h2,{id:\"hello-world\",children:\"Hello world\"}),`\n`,(0,e.jsx)(t.p,{children:`This is my first post!\\r\n... rest of the content`})]})}function d(n={}){let{wrapper:t}=n.components||{};return t?(0,e.jsx)(t,{...n,children:(0,e.jsx)(a,{...n})}):a(n)}return M(g);})();\n;return Component;",
+    "prev": null,
+    "next": null,
+    "readTime": 1
+  }
+]
